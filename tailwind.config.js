65c6afa301b77d29eb6af70e2719244f4c625cc5
YourUsername <ismoilzainalov@gmail.com>
2024-11-29 19:56:11 +0300

    end of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       'sm': '0px',
       // => @media (min-width: 640px) { ... }
 
-      'md': '510px',
+      'md': '768px',
       // => @media (min-width: 768px) { ... }
 
       'lg': '1024px',
